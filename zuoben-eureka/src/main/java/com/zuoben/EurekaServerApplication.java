aae8be294cf0a41946bdb0456c88aa7d33ca21ae
左本 <1214237256@qq.com>
2018-12-27 07:47:10 +0000

    工程第一次提交

---
+++ zuoben-eureka/src/main/java/com/zuoben/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package com.zuoben;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务注册中心
+ *
+ * @author zuoben
+ * @create 2018-07-19
+ *
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
