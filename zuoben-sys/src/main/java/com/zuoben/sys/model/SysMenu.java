c5dd9c2c86115bb57cad92a263893217639a4696
左本 <1214237256@qq.com@21dc5f14-086d-4dcb-850c-1ee93fdc3e19>
2018-12-28 08:10:16 +0000

    项目整体优化

---
+++ zuoben-sys/src/main/java/com/zuoben/sys/model/SysMenu.java
@@ -0,0 +1,138 @@
+package com.zuoben.sys.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class SysMenu implements Serializable {
+    private Integer id;
+
+    private String text;
+
+    private String href;
+
+    private Integer parentId;
+
+    private String icon;
+
+    private Integer type;
+
+    private String path;
+
+    private Integer sort;
+
+    private Integer state;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Integer createId;
+
+    private Integer updateId;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text == null ? null : text.trim();
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href == null ? null : href.trim();
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon == null ? null : icon.trim();
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path == null ? null : path.trim();
+    }
+
+    public Integer getSort() {
+        return sort;
+    }
+
+    public void setSort(Integer sort) {
+        this.sort = sort;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getCreateId() {
+        return createId;
+    }
+
+    public void setCreateId(Integer createId) {
+        this.createId = createId;
+    }
+
+    public Integer getUpdateId() {
+        return updateId;
+    }
+
+    public void setUpdateId(Integer updateId) {
+        this.updateId = updateId;
+    }
+}
